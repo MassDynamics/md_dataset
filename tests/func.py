a24de45f66773070b43ad7911a384c3cf9d7e69e
YANSIN <yansin.huang1@unimelb.edu.au>
2025-10-02 14:37:34 +1000

    linting

--- tests/func.py
+++ tests/func.py
@@ -15,6 +15,6 @@ def test_func(input_datasets: list[InputDataset], params: TestBlahParams, \
 
 @md_py
 def test_func_properties(input_datasets: list[InputDataset], params: TestBlahParams, \
-        output_dataset_type: DatasetType) -> dict:
+        output_dataset_type: DatasetType) -> dict: # noqa: ARG001
     """A nice description."""
     return {}
