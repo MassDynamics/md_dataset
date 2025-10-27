abaf5b9087c504e231d3f039f3f7a182faf4359c
Anna Quaglieri <anna.quaglieri16@gmail.com>
2025-10-27 17:07:57 +1100

    try add PTM in the required tables

--- src/md_dataset/models/dataset.py
+++ src/md_dataset/models/dataset.py
@@ -129,7 +129,7 @@ class IntensityDataset(Dataset):
             msg = f"The field 'intensity_tables' must be a list, but got {type(intensity_tables).__name__}."
             raise TypeError(msg)
 
-        required_table_types = {IntensityTableType.INTENSITY, IntensityTableType.METADATA}
+        required_table_types = {IntensityTableType.INTENSITY, IntensityTableType.METADATA, IntensityTableType.PTM_SITES}
 
         for i, datum in enumerate(intensity_tables):
             if not isinstance(datum, IntensityData):
